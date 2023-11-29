05453528e433f8f720698c3e5524533647937e4e
Cedric <cedigasser@gmail.com>
2023-11-29 15:07:05 +0100

    Add new classes

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,44 +1,54 @@
 <script lang="ts">
-	import '$lib/assets/css/reset.css'
-	import '$lib/assets/css/app.css';
+  import '$lib/assets/css/reset.css'
+  import '$lib/assets/css/app.css'
 </script>
 
-<main>
-	<slot />
-</main>
+<div class="theme-rose-pine">
+  <main>
+    <slot />
+  </main>
 
-<footer>
-	<p>Code can be found <a href="https://github.com/CediGasser/gfuscht">here</a>.</p>
-</footer>
+  <footer>
+    <p>
+      Code can be found <a href="https://github.com/CediGasser/gfuscht">here</a>
+    </p>
+  </footer>
+</div>
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-		min-height: 100vh;
-	}
+  div {
+    background-color: var(--theme-base);
+    color: var(--theme-text);
+  }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    box-sizing: border-box;
+    min-height: 100vh;
+  }
 
-	footer a {
-		font-weight: bold;
-	}
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+    background-color: var(--theme-surface);
+  }
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
+  footer a {
+    font-weight: bold;
+  }
+
+  @media (min-width: 480px) {
+    footer {
+      padding: 40px 0;
+    }
+  }
 </style>
