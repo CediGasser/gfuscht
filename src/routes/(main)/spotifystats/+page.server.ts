fd08c13da3996c3ec3fb30a0ead0feb510e07f6a
Cedric <cedigasser@gmail.com>
2023-12-17 12:48:00 +0100

    Refactor access token checking

--- src/routes/(main)/spotifystats/+page.server.ts
+++ src/routes/(main)/spotifystats/+page.server.ts
@@ -3,24 +3,32 @@ import { redirect } from "@sveltejs/kit"
 
 export const load = async ({ cookies, setHeaders }) => {
     let accessToken = cookies.get('spotify_token')
+    let refreshToken = cookies.get('spotify_refresh_token')
+    setHeaders({
+        'Cache-Control': 'private, max-age=0, no-cache'
+    })
 
+    // redirect to login if no tokens are present
+    if (!accessToken && !refreshToken) {
+        throw redirect(301, '/spotifystats/login')
+    }
+
+    // get new access token if only refresh token is present
     if (!accessToken) {
-        let refreshToken = cookies.get('spotify_refresh_token')
-        if (!refreshToken) {
-            setHeaders({
-                'Cache-Control': 'private, max-age=0, no-cache'
-            })
+        let token = await getAccessToken({refreshToken})
+
+        if (!token) {
+            // if refresh token is invalid, delete cookies and redirect to login
+            cookies.delete('spotify_token')
+            cookies.delete('spotify_refresh_token')
             throw redirect(301, '/spotifystats/login')
         }
 
-        accessToken = (await getAccessToken({refreshToken}))?.access_token
-    }
-
-    if (!accessToken) {
-        setHeaders({
-            'Cache-Control': 'private, max-age=0, no-cache'
+        cookies.set('spotify_token', token.access_token, {
+            maxAge: token.expires_in,
+            path: '/'
         })
-        throw redirect(301, '/spotifystats/login')
+        accessToken = token.access_token
     }
 
     return {
