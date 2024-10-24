40eff7f59670a03311ee49cfbe2c940440c82cf3
Cedric <cedigasser@gmail.com>
2024-10-24 19:04:05 +0200

    Added Chart.svelte and data.json

---
+++ src/routes/(standalone)/kivis/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  // import Chart from "./Chart.svelte";
+  import data from "./data.json";
+
+  console.log(data);
+</script>
+
+<main>
+  <blockquote>"A new relationship between art and science."</blockquote>
+  <p>Albert-László Barabási</p>
+  <!-- <Chart {data} /> -->
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #f0f0f0;
+    color: #333;
+    font-family: sans-serif;
+  }
+  blockquote {
+    font-size: 2rem;
+    font-weight: 300;
+    text-align: center;
+  }
+  p {
+    font-size: 1rem;
+    font-weight: 300;
+    text-align: center;
+  }
+</style>
