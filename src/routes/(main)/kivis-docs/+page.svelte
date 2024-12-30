bed77697863eebeb89611b3ef3798e4a14eeec50
Cedric <cedigasser@gmail.com>
2024-12-30 22:58:07 +0100

    Added initial kivis-docs

---
+++ src/routes/(main)/kivis-docs/+page.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+  import task_1a1 from './assets/1-a-1.png'
+  import task_1a2 from './assets/1-a-2.png'
+  import task_1a3 from './assets/1-a-3.png'
+  import task_1b1 from './assets/1-b-1.png'
+  import task_1b2 from './assets/1-b-2.png'
+  import task_1b3 from './assets/1-b-3.png'
+  import task_1c1 from './assets/1-c-1.png'
+  import task_1c2 from './assets/1-c-2.png'
+  import task_1c3 from './assets/1-c-3.png'
+  import task_1d1 from './assets/1-d-1.png'
+  import task_1d2 from './assets/1-d-2.png'
+  import task_1d3 from './assets/1-d-3.png'
+  import task_2 from './assets/figur-und-grund.png'
+  import task_3 from './assets/Typography-Comparison.png'
+</script>
+
+<main>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">KiVis</h1>
+      </div>
+    </div>
+  </section>
+  <section class="task-1">
+    <h2 class="title">Position der Formen</h2>
+    <div class="tasks">
+      <h3>A</h3>
+      <div class="flex-row">
+        <img src={task_1a1} alt="1-a-1" />
+        <img src={task_1a2} alt="1-a-2" />
+        <img src={task_1a3} alt="1-a-3" />
+      </div>
+      <h3>B</h3>
+      <div class="flex-row">
+        <img src={task_1b1} alt="1-b-1" />
+        <img src={task_1b2} alt="1-b-2" />
+        <img src={task_1b3} alt="1-b-3" />
+      </div>
+      <h3>C</h3>
+      <div class="flex-row">
+        <img src={task_1c1} alt="1-c-1" />
+        <img src={task_1c2} alt="1-c-2" />
+        <img src={task_1c3} alt="1-c-3" />
+      </div>
+      <h3>D</h3>
+      <div class="flex-row">
+        <img src={task_1d1} alt="1-d-1" />
+        <img src={task_1d2} alt="1-d-2" />
+        <img src={task_1d3} alt="1-d-3" />
+      </div>
+    </div>
+  </section>
+  <section class="task-2">
+    <div class="container">
+      <h2 class="title">Figur und Grund</h2>
+      <img src={task_2} alt="figur und grund" />
+    </div>
+  </section>
+  <section class="task-3">
+    <div class="container">
+      <h2 class="title">Schriftvergleich</h2>
+      <img src={task_3} alt="Typographie Vergleich" />
+    </div>
+  </section>
+  <section class="task-4">
+    <div class="container">
+      <h2 class="title">Schriftsatz</h2>
+      <p></p>
+    </div>
+  </section>
+</main>
+
+<style>
+  main {
+    width: 100%;
+    min-height: 100vh;
+  }
+
+  section {
+    padding: 2rem;
+    margin: 0 auto;
+    max-width: 800px;
+  }
+
+  h1 {
+    font-size: 6rem;
+    font-weight: 700;
+  }
+
+  h2 {
+    font-size: 4rem;
+    font-weight: 700;
+  }
+
+  h3 {
+    font-size: 3rem;
+    font-weight: 700;
+  }
+
+  p {
+    font-size: 2rem;
+    font-weight: 400;
+  }
+
+  .flex-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .task-1 img {
+    width: 30%;
+  }
+</style>
