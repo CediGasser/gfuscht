2b8e9b82ef0a8b905f1c36d8c7926d82dd58e3da
Cedric <cedigasser@gmail.com>
2025-01-03 23:00:31 +0100

    Added kivis navigation

---
+++ src/routes/(standalone)/kivis/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+  import { goto } from '$app/navigation'
+
+  interface Props {
+    children?: Snippet
+  }
+  let { children }: Props = $props()
+
+  const onNavigate = (event: KeyboardEvent) => {
+    console.log(event.key)
+    if (event.key >= '1' && event.key <= '6') {
+      event.preventDefault()
+      goto(`/kivis/v${event.key}`)
+    }
+  }
+</script>
+
+<svelte:body onkeypress={onNavigate} />
+
+{@render children?.()}
