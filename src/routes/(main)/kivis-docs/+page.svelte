a240dc85e313f13dec4c20c2de486ec3cc1ba439
Cedric <cedigasser@gmail.com>
2025-01-03 05:42:09 +0100

    Added Lightbox component

--- src/routes/(main)/kivis-docs/+page.svelte
+++ src/routes/(main)/kivis-docs/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Lightbox from './Lightbox.svelte'
+
   import task_1a1 from './assets/1-a-1.png'
   import task_1a2 from './assets/1-a-2.png'
   import task_1a3 from './assets/1-a-3.png'
@@ -42,6 +44,8 @@
   import task_11_5_3 from './assets/drawings/5-3.jpeg'
   import task_11_5_4 from './assets/drawings/5-4.jpeg'
   import task_11_5_5 from './assets/drawings/5-5.jpeg'
+  import ListColumn from '../clock/ListColumn.svelte'
+  import Layout from '../+layout.svelte'
 </script>
 
 <main>
@@ -56,41 +60,53 @@
     <h2 class="title">Position der Formen</h2>
     <div class="tasks">
       <h3>A</h3>
-      <div class="flex-row">
-        <img src={task_1a1} alt="1-a-1" />
-        <img src={task_1a2} alt="1-a-2" />
-        <img src={task_1a3} alt="1-a-3" />
-      </div>
+      <Lightbox>
+        <div class="flex-row">
+          <img src={task_1a1} alt="1-a-1" />
+          <img src={task_1a2} alt="1-a-2" />
+          <img src={task_1a3} alt="1-a-3" />
+        </div>
+      </Lightbox>
       <h3>B</h3>
-      <div class="flex-row">
-        <img src={task_1b1} alt="1-b-1" />
-        <img src={task_1b2} alt="1-b-2" />
-        <img src={task_1b3} alt="1-b-3" />
-      </div>
+      <Lightbox>
+        <div class="flex-row">
+          <img src={task_1b1} alt="1-b-1" />
+          <img src={task_1b2} alt="1-b-2" />
+          <img src={task_1b3} alt="1-b-3" />
+        </div>
+      </Lightbox>
       <h3>C</h3>
-      <div class="flex-row">
-        <img src={task_1c1} alt="1-c-1" />
-        <img src={task_1c2} alt="1-c-2" />
-        <img src={task_1c3} alt="1-c-3" />
-      </div>
+      <Lightbox>
+        <div class="flex-row">
+          <img src={task_1c1} alt="1-c-1" />
+          <img src={task_1c2} alt="1-c-2" />
+          <img src={task_1c3} alt="1-c-3" />
+        </div>
+      </Lightbox>
       <h3>D</h3>
-      <div class="flex-row">
-        <img src={task_1d1} alt="1-d-1" />
-        <img src={task_1d2} alt="1-d-2" />
-        <img src={task_1d3} alt="1-d-3" />
-      </div>
+      <Lightbox>
+        <div class="flex-row">
+          <img src={task_1d1} alt="1-d-1" />
+          <img src={task_1d2} alt="1-d-2" />
+          <img src={task_1d3} alt="1-d-3" />
+        </div>
+      </Lightbox>
     </div>
   </section>
   <section class="task-2">
     <div class="container">
       <h2 class="title">Figur und Grund</h2>
-      <img src={task_2} alt="figur und grund" />
+      <Lightbox>
+        <img src={task_2} alt="figur und grund" />
+      </Lightbox>
     </div>
   </section>
   <section class="task-3">
     <div class="container">
       <h2 class="title">Schriftvergleich</h2>
-      <img src={task_3} alt="Typographie Vergleich" />
+      <Lightbox>
+        <img src={task_3} alt="Typographie Vergleich" />
+      </Lightbox>
     </div>
   </section>
   <section class="task-4">
@@ -101,21 +117,43 @@
   <section class="task-5">
     <div class="container">
       <h2 class="title">Interaktion von Farben</h2>
-      <img src={task_5_1} alt="Interaction of Color 1" class="bg-white p-2" />
-      <img src={task_5_2} alt="Interaction of Color 2" class="bg-white p-2" />
-      <img src={task_5_3} alt="Interaction of Color 3" class="bg-white p-2" />
+      <Lightbox>
+        <img
+          src={task_5_1}
+          alt="Interaction of Color 1"
+          class="bg-white p-2 my-2"
+        />
+      </Lightbox>
+      <Lightbox>
+        <img
+          src={task_5_2}
+          alt="Interaction of Color 2"
+          class="bg-white p-2 my-2"
+        />
+      </Lightbox>
+      <Lightbox>
+        <img
+          src={task_5_3}
+          alt="Interaction of Color 3"
+          class="bg-white p-2 my-2"
+        />
+      </Lightbox>
     </div>
   </section>
   <section class="task-6">
     <div class="container">
       <h2 class="title">Hierarchie</h2>
-      <img src={task_6} alt="Visual Hierarchy" class="bg-white" />
+      <Lightbox>
+        <img src={task_6} alt="Visual Hierarchy" />
+      </Lightbox>
     </div>
   </section>
   <section class="task-7">
     <div class="container">
       <h2 class="title">Whitespace</h2>
-      <img src={task_7} alt="Whitespace" class="bg-white" />
+      <Lightbox>
+        <img src={task_7} alt="Whitespace" />
+      </Lightbox>
     </div>
   </section>
   <section class="task-8">
@@ -126,8 +164,12 @@
   <section class="task-9">
     <div class="container">
       <h2 class="title">Bildbetrachtung</h2>
-      <img src={task_9_1} alt="Ottojaja" />
-      <img src={task_9_2} alt="Festival des Arts" />
+      <Lightbox>
+        <img src={task_9_1} alt="Ottojaja" class="my-2" />
+      </Lightbox>
+      <Lightbox>
+        <img src={task_9_2} alt="Festival des Arts" class="my-2" />
+      </Lightbox>
     </div>
   </section>
   <section class="task-10">
@@ -136,19 +178,35 @@
     </div>
   </section>
   <section class="task-11">
-    <div class="container">
-      <h2 class="title">Zeichnen</h2>
-      <img src={task_11_1} alt="Hand" class="bg-white p-2" />
-      <img src={task_11_2} alt="Perspektive" class="bg-white p-2" />
-      <img src={task_11_3} alt="Kopf" class="bg-white p-2" />
-      <img src={task_11_4} alt="Figur" class="bg-white p-2" />
-      <div class="flex-row">
-        <img src={task_11_5_1} alt="5-1" class="bg-white p-2 m-2" />
-        <img src={task_11_5_2} alt="5-2" class="bg-white p-2 m-2" />
-        <img src={task_11_5_3} alt="5-3" class="bg-white p-2 m-2" />
-        <img src={task_11_5_4} alt="5-4" class="bg-white p-2 m-2" />
-        <img src={task_11_5_5} alt="5-5" class="bg-white p-2 m-2" />
-      </div>
+    <h2 class="title">Zeichnen</h2>
+    <Lightbox>
+      <img src={task_11_1} alt="Hand" class="bg-white my-2" />
+    </Lightbox>
+    <Lightbox>
+      <img src={task_11_2} alt="Perspektive" class="bg-white my-2" />
+    </Lightbox>
+    <Lightbox>
+      <img src={task_11_3} alt="Kopf" class="bg-white my-2" />
+    </Lightbox>
+    <Lightbox>
+      <img src={task_11_4} alt="Figur" class="bg-white my-2" />
+    </Lightbox>
+    <div class="flex-row drawings my-2">
+      <Lightbox>
+        <img src={task_11_5_1} alt="5-1" />
+      </Lightbox>
+      <Lightbox>
+        <img src={task_11_5_2} alt="5-2" />
+      </Lightbox>
+      <Lightbox>
+        <img src={task_11_5_3} alt="5-3" />
+      </Lightbox>
+      <Lightbox>
+        <img src={task_11_5_4} alt="5-4" />
+      </Lightbox>
+      <Lightbox>
+        <img src={task_11_5_5} alt="5-5" />
+      </Lightbox>
     </div>
   </section>
 </main>
@@ -157,6 +215,10 @@
   main {
     width: 100%;
     min-height: 100vh;
+  }
+
+  img {
+    object-fit: contain;
   }
 
   section {
@@ -206,5 +268,17 @@
 
   .m-2 {
     margin: 2rem;
+  }
+
+  .my-2 {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  .drawings img {
+  }
+
+  .drawings {
+    gap: 1rem;
   }
 </style>
---
+++ src/routes/(main)/kivis-docs/Lightbox.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+  import { fade } from 'svelte/transition'
+
+  interface Props {
+    children?: Snippet
+  }
+  let { children }: Props = $props()
+
+  let isOpen = $state(false)
+
+  const open = (e: any) => {
+    if (e.type === 'keypress' && e.key !== 'Enter') return
+    isOpen = !isOpen
+  }
+</script>
+
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div class="lightbox" onclick={open} onkeypress={open}>
+  <div class="lightbox__content">
+    {@render children?.()}
+  </div>
+  {#if isOpen}
+    <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+    <div
+      class="overlay"
+      transition:fade={{ duration: 200 }}
+      tabindex="0"
+      onblur={() => (isOpen = false)}
+    >
+      <div class="overlay__content">
+        {@render children?.()}
+      </div>
+    </div>
+  {/if}
+</div>
+
+<style>
+  .lightbox:hover {
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.2);
+    cursor: zoom-in;
+  }
+
+  .overlay {
+    cursor: zoom-out;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1000;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+
+    background-color: color-mix(
+      in srgb,
+      var(--theme-base),
+      rgba(0, 0, 0, 0.98)
+    );
+  }
+
+  .overlay__content {
+    padding: 1rem;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
+    object-fit: scale-down;
+    max-width: 80%;
+    max-height: 80%;
+    overflow: auto;
+    display: flex;
+  }
+</style>
