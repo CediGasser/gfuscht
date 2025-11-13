d202bfdb76035697d682442824521e516ffe67b1
Cedric <cedigasser@gmail.com>
2025-11-13 21:28:52 +0100

    Added basic ict-bz logo

---
+++ src/routes/(main)/ict-bz/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
+</script>
+
+<main>
+  <Canvas>
+    <Scene />
+  </Canvas>
+</main>
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+</style>
---
+++ src/routes/(main)/ict-bz/Cube.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+
+  interface Props {
+    position: { x: number; y: number }
+    color?: string
+  }
+  let { position = { x: 0, y: 0 }, color = undefined }: Props = $props()
+</script>
+
+<T.Mesh position.x={position.x} position.y={position.y} position.z={0}>
+  <T.BoxGeometry />
+  <T.MeshStandardMaterial color={color ?? '#00ADEF'} />
+</T.Mesh>
